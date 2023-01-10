96da1ca08e01d98591904c39e2be981bc2a26c90
Chr0110 <eradi-@student.1337.ma>
2023-01-10 14:29:37 +0100

    delete

--- cpp/cpp01/ex00/newZombie.cpp
+++
@@ -1,7 +0,0 @@
-#include "Zombie.hpp"
-
-Zombie* newZombie(std::string name)
-{
-	 //create a zombie and return it to use it outside the scope  (HEAP)
-	return (new Zombie(name));
-}
--- cpp/cpp01/ex00/randomChamp.cpp
+++
@@ -1,8 +0,0 @@
-#include "Zombie.hpp"
-
-void randomChump(std::string name)
-{
-    //create a zombie the the zombie annonced its self (STACK)
-    Zombie zmb(name);
-    zmb.annonce();
-}
